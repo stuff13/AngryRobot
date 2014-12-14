5c4a3749f5ad5901bfb43ab1c3d154076dc9fa40
Michael MacKinnon <mackinnon.m@gmail.com>
2014-12-14 22:05:10 +0000

    initial state

---
+++ Assets/Scripts/Build.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+
+public class BuildScript{
+	
+	static string[] SCENES = FindEnabledEditorScenes();
+	
+	static string APP_NAME = "<yourAPPName>";
+	static string TARGET_DIR = "<YourTargetDir>";
+	
+	[MenuItem ("Custom/CI/Build Mac OS X")]
+	static void PerformMacOSXBuild ()
+	{
+		string target_dir = APP_NAME + ".app";
+		GenericBuild(SCENES, TARGET_DIR + "/" + target_dir, BuildTarget.StandaloneOSXIntel,BuildOptions.None);
+	}
+	
+//	[MenuItem ("Custom/CI/Build Web")]
+//	static void PerformWebBuild ()
+//	{
+//		string target_dir = APP_NAME + ".unity3d";
+//		GenericBuild(SCENES, TARGET_DIR + "/web/" + target_dir, BuildTarget.WebPlayer,BuildOptions.None);
+//	}
+//	
+	private static string[] FindEnabledEditorScenes() {
+		List&lt;string&gt; EditorScenes = new List&lt;string&gt;();
+		foreach(EditorBuildSettingsScene scene in EditorBuildSettings.scenes) {
+			if (!scene.enabled) continue;
+			EditorScenes.Add(scene.path);
+		}
+		return EditorScenes.ToArray();
+	}
+	
+	static void GenericBuild(string[] scenes, string target_dir, BuildTarget build_target, BuildOptions build_options)
+	{
+		EditorUserBuildSettings.SwitchActiveBuildTarget(build_target);
+		string res = BuildPipeline.BuildPlayer(scenes,target_dir,build_target,build_options);
+		if (res.Length &gt; 0) {
+			throw new Exception("BuildPlayer failure: " + res);
+		}
+	}
+	
+}
